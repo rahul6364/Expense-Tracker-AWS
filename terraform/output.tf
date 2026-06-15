eb5490c3908cc0249443576bcf8fcaf8100fc6a7
rahul6364 <rkulal6177@gmail.com>
2026-06-15 23:23:41 +0530

    test terraform-pr  workflow

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,6 @@
 output "rds_endpoint" {
   description = "RDS MySQL hostname (private)"
-  value       = aws_db_instance.mysql_rds.address
+  value       = aws_db_instance.mysql_rds.address  #gives the private url of the database
 }
 
 output "alb_dns_name" {
@@ -10,5 +10,5 @@ output "alb_dns_name" {
 
 output "application_url" {
   description = "URL to open the expense tracker in a browser"
-  value       = "http://${aws_lb.expense_alb.dns_name}/"
+  value       = "http://${aws_lb.expense_alb.dns_name}/"  #gives the public url of the application
 }
