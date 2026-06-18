ef8f0a9ce31531d451726a22da1adfdf04e59490
rahul6364 <rkulal6177@gmail.com>
2026-06-18 23:19:09 +0530

    Fix: Added the require version section

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~> 6.0"
     }
   }
-
+  required_version = ">= 1.10.0"
   backend "s3" {
     bucket       = "rahul-expense-tracker-tf-state"
     key          = "terraform/terraform.tfstate"
